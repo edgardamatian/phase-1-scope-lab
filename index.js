36a0ecd3ca5052152210e0483f214fad71a78ab3
Edgar Damatian <damatian.e@gmail.com>
2024-01-31 19:03:18 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+var customerName = 'bob'; // Declare and assign customerName in the global scope
+
+function getCustomerName() {
+  return customerName; // Return the value of customerName from the global scope
+}
+
+function upperCaseCustomerName() {
+ customerName = 'BOB'; // Modify customerName to be 'BOB'
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+
+  const leastFavoriteCustomer = 'John';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Jane';
+}
+
+// console.log(getCustomerName()); // Output: 'bob'
+
+// upperCaseCustomerName(); // Call the function to modify the customerName variable
+
+// console.log(getCustomerName()); // Output: 'BOB'
